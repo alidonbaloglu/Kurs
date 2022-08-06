89c7dc251e494b26138c7c041e4eb678c7b0ff7d
Ali Donbaloğlu <72136581+alidonbaloglu@users.noreply.github.com>
2022-08-06 19:29:07 +0300

    Add project files.

---
+++ Kurs/Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kurs
+{
+    internal class Main
+    {
+        readonly Urun urun1 = new Urun();
+        
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Kurs/Program.cs
@@ -0,0 +1,14 @@
+﻿class Urun
+{
+    public int Id { get; set; }
+    public string? Adi { get; set; }
+    public int Fiyat { get; set; }
+    public string? Aciklama { get; set; }
+
+}
+
+new Urun().Adi = "elma";
+new Urun().Aciklama = "elamlar cok güzel";
+new Urun().Fiyat = 45;
+
+Console.WriteLine(new Urun().Adi);
